de208c3eed1d33b85a35081b585b47c6fa3945df
Prajval Bhale <prajbhale007@gmail.com>
2023-01-20 19:01:54 +0530

    Add @Repository annotation
    Add @Repository annotation, also check Query, Query was wrong

--- src/main/java/com/example/demo/AdminRepo.java
+++ src/main/java/com/example/demo/AdminRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@Repository
 public interface AdminRepo extends JpaRepository<Admin, Integer>{
 
 	public Admin ifExits(Admin a);
